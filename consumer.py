13e447b1353a73dd85ce7139f576fd4424cc3c55
exequielmoneva <exequielmoneva@gmail.com>
2021-09-12 04:12:47 -0300

    - Likes implemented - producer implemented - README.md updated

--- consumer.py
+++ consumer.py
@@ -1,4 +1,12 @@
+import json
 import pika
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
 
 params = pika.URLParameters('amqps://rinseenq:iZkfHfgARAZ5YlARbX6yKDS0yYhxcn-X@beaver.rmq.cloudamqp.com/rinseenq')
 #   params = pika.URLParameters('localhost')
@@ -12,7 +20,11 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
     print('Received in admin')
-    print(body)
+    _id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=_id)
+    product.likes += 1
+    product.save()
 
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
